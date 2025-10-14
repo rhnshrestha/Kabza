e2bdc1e58e773a5580e56b01fd8f2d235712ff17
rhnshrestha <shresthrohan447@gmail.com>
2025-10-14 23:14:02 +0545

    created bookingForm controller, yet to test in postman

---
+++ server/controller/bookingFormController.js
@@ -0,0 +1,31 @@
+const {Booking, Table, BookingStatus} = require('../database/connect');
+const createBooking = async (req, res)=>{
+    try {
+        const {customer_name, contact, booking_date, booking_time, people} = req.body;
+        if(!customer_name || !contact || !booking_date || !booking_time || !people){
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+        const newBooking = await Booking.create({
+            customer_name,
+            contact,
+            booking_date,
+            booking_time,
+            people,
+            table_id: null,
+            status_id: 1
+        });
+
+        res.status(201).json({
+            message: "booking form submitted successfully! status:pending",
+            booking: newBooking
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            message: "server error"
+        });
+    }
+}
