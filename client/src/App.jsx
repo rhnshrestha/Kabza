22b7c079d9b6a6a71caca3921d2a95559f097711
rhnshrestha <shresthrohan447@gmail.com>
2025-10-29 21:30:28 +0545

    routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {Route, Routes, BrowserRouter} from "react-router-dom"
+import BookingFormTable from "./pages/BookingFormPage"
 
 function App() {
 
   return (
-    <>
-      <h1>Book Your Table</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<BookingFormTable/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/BookingFormPage.jsx
@@ -0,0 +1,9 @@
+function BookingFormPage(){
+    return(
+        <>
+            <h1>this page contains booking form</h1>
+        </>
+    )
+}
+
+export default BookingFormPage
